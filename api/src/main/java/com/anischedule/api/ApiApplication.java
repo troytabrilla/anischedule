6bf48522bb279cc80f74d6c1c50f9f9b36ea23aa
Troy Tabrilla <troy.tabrilla@gmail.com>
2025-08-11 18:21:54 -0700

    add cors for local ui services

--- api/src/main/java/com/anischedule/api/ApiApplication.java
+++ api/src/main/java/com/anischedule/api/ApiApplication.java
@@ -2,18 +2,38 @@ package com.anischedule.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @RestController
 @SpringBootApplication
 @ComponentScan(basePackages = "com.anischedule")
 public class ApiApplication {
 
-    @RequestMapping("/")
+    private final String[] allowedOrigins = new String[]{
+        "http://localhost:5173", // vite server
+        "http://localhost:3000" // express server
+    };
+
+    @GetMapping("/")
     public String home() {
         return "AniSchedule API";
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/").allowedOrigins(allowedOrigins);
+                registry.addMapping("/anime").allowedOrigins(allowedOrigins);
+            }
+        };
     }
 
     public static void main(String[] args) {
